344ead061d3112cb7d6874cd82c7429ba41ff79b
David Blbulyan <blbulyandavbulyan@gmail.com>
2024-03-17 13:38:20 +0400

    added Counter

---
+++ app/src/main/java/com/blbulyandavbulan/passengercounter/Counter.kt
@@ -0,0 +1,23 @@
+package com.blbulyandavbulan.passengercounter
+
+import android.widget.TextView
+
+class Counter(private val textView: TextView) {
+    var value = 0
+        private set
+
+    init {
+        textView.text = "0"
+    }
+
+    fun inc() {
+        value++
+        textView.text = value.toString()
+    }
+
+    fun dec() {
+        if (value > 0) value--
+    }
+
+    fun isMinimum(): Boolean = value == 0
+}
