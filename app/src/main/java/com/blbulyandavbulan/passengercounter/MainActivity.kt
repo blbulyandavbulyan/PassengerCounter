bc7365221ab30faf8a419a672271dc076eb25b4a
David Blbulyan <blbulyandavbulyan@gmail.com>
2024-03-17 16:44:05 +0400

    implemented MainActivity#onCreate method with all necessary glue logic

--- app/src/main/java/com/blbulyandavbulan/passengercounter/MainActivity.kt
+++ app/src/main/java/com/blbulyandavbulan/passengercounter/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.blbulyandavbulan.passengercounter
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 import com.blbulyandavbulan.passengercounter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +10,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val maxPlacesCount = 50
+        val counter = PlacesCounter(maxPlacesCount) { binding.counterTextView.text = it.toString() }
+        val detailsDisplay = DetailsDisplay(binding.detailsTextView, counter)
+        val decButton = binding.decButton
+        val resetButton = binding.resetButton
+        decButton.isEnabled = false
+        decButton.setOnClickListener {
+            counter.dec()
+            detailsDisplay.update()
+            if (counter.isMinReached()) {
+                decButton.isEnabled = false;
+            }
+            if (!counter.isMaxReached()) {
+                resetButton.isVisible = false
+            }
+        }
+        binding.incButton.setOnClickListener {
+            counter.inc()
+            decButton.isEnabled = true
+            detailsDisplay.update()
+            if (counter.isMaxReached()) {
+                resetButton.isVisible = true
+            }
+        }
+        resetButton.isVisible = false;
+        resetButton.setOnClickListener {
+            counter.reset()
+            decButton.isEnabled = false
+            resetButton.isVisible = false;
+            detailsDisplay.update()
+        }
     }
 }
