a25256f42612b5725ac3302a30715fa394aa8ef5
David Blbulyan <blbulyandavbulyan@gmail.com>
2024-03-17 10:54:39 +0400

    initial commit

---
+++ app/src/main/java/com/blbulyandavbulan/passengercounter/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.blbulyandavbulan.passengercounter
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.blbulyandavbulan.passengercounter.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
